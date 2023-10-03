ba38a90716e4612a731f756f8b5bbc9f4838621d
adulbasit <bassitb50@gmail.com>
2023-10-03 18:34:10 +0500

    Complete

---
+++ Simple_Calculator-Task2.cpp
@@ -0,0 +1,31 @@
+//This is my first task of CODSOFT-Internship
+//Name : ABDUL BASIT
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	char X;
+	while (true){
+	cout<<"\nEnter the first Number: ";
+	cin>>num1;
+	cout<<"Enter the Second Number: ";
+	cin>>num2;
+	cout<<"\nPress a for addition.\n";
+	cout<<"Press b for subtraction.\n";
+	cout<<"Press c for Multiplication.\n";
+	cout<<"Press d for division.\n";
+	cin>>X;
+	if(X=='a')
+	cout<<num1+num2;
+	else if(X=='b')
+	cout<<num1-num2;
+	else if(X=='c')
+	cout<<num1*num2;
+	else if(X=='d')
+	cout<<num1/num2;
+	else
+	cout<<"Invalid Operation";}
+}
